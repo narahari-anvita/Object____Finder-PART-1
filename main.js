5019e358dcb1bd33df3827a831703c5d32aa26b1
narahari-anvita <79611794+narahari-anvita@users.noreply.github.com>
2022-03-02 14:16:55 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+    
+}
+
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    console.log("Webcam Made It!")
+}
+
+function draw(){
+    image(video,0,0,380,380);
+}
